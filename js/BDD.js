f3ed28f8d8a0941ba6830951bfdb917271a41c96
stampil <developpeurntic@gmail.com>
2014-10-21 16:55:53 +0200

    v1.0.0

--- js/BDD.js
+++ js/BDD.js
@@ -19,11 +19,12 @@ function init(){
     db.transaction(function (tx) {
         tx.executeSql('CREATE TABLE IF NOT EXISTS dictionnaire\n\
                              (mot VARCHAR)');
-        setTimeout(testTable,200);
+        setTimeout(testTable,20);
         setTimeout(function(){
-            initGame()
+            console.log("INIT_GAME");
+            initGame();
             getWord(1);
-        },300);
+        },29);
         
     });
 }
@@ -67,7 +68,7 @@ function fillTable() {
             //console.log("too late");
             return;
         }
-        console.log(dictionnaire[inc_dico],inc_dico);
+        
         tx.executeSql(
             'INSERT INTO dictionnaire (mot) VALUES (?)',
             [dictionnaire[inc_dico]],
@@ -80,13 +81,18 @@ function fillTable() {
 function tableCreate() {
     document.getElementById('loading_dico').style.display = "block";
     document.getElementById("nb_loaded").textContent = Math.round(inc_dico/dictionnaire.length*100)+'%';
-    interval_dico = setInterval(fillTable, 10);
+    if(interval_dico){ 
+        clearInterval(interval_dico);
+        interval_dico = null;
+    }
+    interval_dico = setInterval(fillTable, 500);
 }
 
 
 function getWord(line) {
     console.log("getWord call",line, new Date().getTime());
     db.transaction(function (tx) {
+        console.log('transaction select word');
         rowid = Math.floor(Math.random() * inc_dico) + 1;
         console.log("getWord send ",line, new Date().getTime(),rowid);
         tx.executeSql(
@@ -105,6 +111,7 @@ function validateWord(tryWord, callback){
          //si le dictionnaire n'est pas encore rempli on valide tout les mots rentré ( temporaire aux premieres parties )
          console.log("check validate word tempory disabled : ",inc_dico,dictionnaire.length-1);
          callback(-1);
+         return;
      }
      db.transaction(function (tx) {
         
